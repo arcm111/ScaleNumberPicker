bc2ef0d7e69fb352533973db5e32440b9a73ebb5
arcm111 <arcm111@hotmail.com>
2021-10-15 17:28:41 +0100

    added demo

--- ScaleNumberPicker/src/main/java/com/arcm/scalenumberpicker/ScaleNumberPicker.java
+++ ScaleNumberPicker/src/main/java/com/arcm/scalenumberpicker/ScaleNumberPicker.java
@@ -20,13 +20,14 @@ import androidx.annotation.Nullable;
 public class ScaleNumberPicker extends View implements ValueAnimator.AnimatorUpdateListener {
     public static final String TAG = "ScaleNumberPicker";
     public final int DEFSTYLE_RES = R.style.ScaleNumberPicker;
-    private final int[] STYLEABLE_RES = R.styleable.ScaleNumberPicker;
+    public static final int HORIZONTAL = 0;
+    public static final int VERTICAL_LEFT = 1;
+    public static final int VERTICAL_RIGHT = 2;
+    public static final int HORIZONTAL_CIRCULAR = 3;
+
+    private static final int[] STYLEABLE_RES = R.styleable.ScaleNumberPicker;
     private static final String EXTRA_VALUE = "scale_value";
     private static final String EXTRA_SUPER_STATE = "super_extra_state";
-    private static final int HORIZONTAL = 0;
-    private static final int VERTICAL_LEFT = 1;
-    private static final int VERTICAL_RIGHT = 2;
-    private static final int HORIZONTAL_CIRCULAR = 3;
 
     private VelocityTracker velocityTracker;
     private float val;
@@ -37,13 +38,14 @@ public class ScaleNumberPicker extends View implements ValueAnimator.AnimatorUpd
 
     private Attributes attrs;
     private ScaleDesign design;
+    private int orientation;
     private OnValueChangedListener listener;
     private ValueAnimator valueAnimator;
 
-    private static final int RANGE_MULTIPLIER = 10;
+    private static final int SWEEP_RANGE_MULTIPLIER = 2;
     private final int MAX_SPEED;
     private final int MIN_SPEED;
-    private int range;
+    private int sweepRange;
 
     public ScaleNumberPicker(Context context) {
         super(context);
@@ -70,16 +72,8 @@ public class ScaleNumberPicker extends View implements ValueAnimator.AnimatorUpd
         // Load attributes
         this.attrs = new Attributes(getContext(), attrSet, STYLEABLE_RES, defStyle, DEFSTYLE_RES);
         this.val = attrs.value;
-        this.range = (int) (attrs.maxValue - attrs.minValue);
-        if (attrs.orientation == HORIZONTAL) {
-            this.design = new HorizontalScale(attrs);
-        } else if (attrs.orientation == VERTICAL_LEFT) {
-            this.design = new VerticalLeftScale(attrs);
-        } else if (attrs.orientation == VERTICAL_RIGHT){
-            this.design = new VerticalRightScale(attrs);
-        } else if (attrs.orientation == HORIZONTAL_CIRCULAR) {
-            this.design = new HorizontalCircularScale(attrs);
-        }
+        this.sweepRange = (int) (attrs.maxValue - attrs.minValue);
+        setOrientation(attrs.orientation);
         this.valueAnimator = ValueAnimator.ofFloat(attrs.minValue, attrs.maxValue);
         valueAnimator.addUpdateListener(this);
         valueAnimator.addListener(new AnimatorListenerAdapter() {
@@ -121,7 +115,7 @@ public class ScaleNumberPicker extends View implements ValueAnimator.AnimatorUpd
         int desiredWidth = design.measureViewContentWidth();
         int desiredHeight = design.measureViewContentHeight();
         
-        if (attrs.orientation == HORIZONTAL || attrs.orientation == HORIZONTAL_CIRCULAR) {
+        if (orientation == HORIZONTAL || orientation == HORIZONTAL_CIRCULAR) {
             desiredWidth = MeasureSpec.getSize(widthMeasureSpec);
         } else desiredHeight = MeasureSpec.getSize(heightMeasureSpec);
 
@@ -135,20 +129,20 @@ public class ScaleNumberPicker extends View implements ValueAnimator.AnimatorUpd
         int contentWidth = width - pLeft - pRight;
         int contentHeight = height - pTop - pBottom;
 
-        if (attrs.orientation == HORIZONTAL) {
+        if (orientation == HORIZONTAL) {
             int margin = (contentWidth / 2) % attrs.subdivisionWidth;
             design.setMargins(pLeft + margin, pTop, pRight + margin, pBottom);
             design.setDimensions(contentWidth - margin * 2, contentHeight);
-            this.range = (int) ((contentWidth / attrs.divisionWidth) * attrs.divisionValue) * RANGE_MULTIPLIER;
-        } else if (attrs.orientation == HORIZONTAL_CIRCULAR) {
+            this.sweepRange = (int) ((contentWidth / attrs.divisionWidth) * attrs.divisionValue) * SWEEP_RANGE_MULTIPLIER;
+        } else if (orientation == HORIZONTAL_CIRCULAR) {
             design.setMargins(pLeft, pTop, pRight, pBottom);
             design.setDimensions(contentWidth, contentHeight);
-            this.range = (int) ((contentWidth / attrs.divisionWidth) * attrs.divisionValue) * RANGE_MULTIPLIER;
+            this.sweepRange = (int) ((contentWidth / attrs.divisionWidth) * attrs.divisionValue) * SWEEP_RANGE_MULTIPLIER;
         } else {
             int margin = (contentHeight / 2) % attrs.subdivisionWidth;
             design.setMargins(pLeft, pTop + margin, pRight, pBottom + margin);
             design.setDimensions(contentWidth, contentHeight - margin * 2);
-            this.range = (int) ((contentHeight / attrs.divisionWidth) * attrs.divisionValue) * RANGE_MULTIPLIER;
+            this.sweepRange = (int) ((contentHeight / attrs.divisionWidth) * attrs.divisionValue) * SWEEP_RANGE_MULTIPLIER;
         }
         design.notifyDimensionsChanged();
 
@@ -204,7 +198,7 @@ public class ScaleNumberPicker extends View implements ValueAnimator.AnimatorUpd
         float dx = event.getX() - dragStartX;
         float dy = event.getY() - dragStartY;
         float newVal = oldVal - (dx / attrs.subdivisionWidth) * attrs.subdivisionValue;
-        if (attrs.orientation == VERTICAL_LEFT || attrs.orientation == VERTICAL_RIGHT) {
+        if (orientation == VERTICAL_LEFT || orientation == VERTICAL_RIGHT) {
             newVal = oldVal + (dy / attrs.subdivisionWidth) * attrs.subdivisionValue;
         }
         this.val = Math.min(Math.max(attrs.minValue, newVal), attrs.maxValue);
@@ -219,10 +213,10 @@ public class ScaleNumberPicker extends View implements ValueAnimator.AnimatorUpd
         // and getYVelocity() to retrieve the velocity for each pointer ID.
         velocityTracker.computeCurrentVelocity(1000);
         float speed = velocityTracker.getXVelocity();
-        if (attrs.orientation == VERTICAL_LEFT || attrs.orientation == VERTICAL_RIGHT) {
+        if (orientation == VERTICAL_LEFT || orientation == VERTICAL_RIGHT) {
             speed = -velocityTracker.getYVelocity();
         }
-        float dv = (speed / MAX_SPEED) * range;
+        float dv = (speed / MAX_SPEED) * sweepRange;
         float end = Math.round((val - dv) / attrs.tickValue) * attrs.tickValue;
         float endValue = Math.max(attrs.minValue, Math.min(end, attrs.maxValue));
         Log.i(TAG, val + ":" + endValue + " max: " + MAX_SPEED + " min: " + MIN_SPEED);
@@ -249,10 +243,6 @@ public class ScaleNumberPicker extends View implements ValueAnimator.AnimatorUpd
         return true;
     }
 
-    public void setOnValueChangedListener(OnValueChangedListener listener) {
-        this.listener = listener;
-    }
-
     @Override
     public void onAnimationUpdate(ValueAnimator animator) {
         if (animator == valueAnimator) {
@@ -268,6 +258,10 @@ public class ScaleNumberPicker extends View implements ValueAnimator.AnimatorUpd
         return Math.round(value / attrs.tickValue) * attrs.tickValue;
     }
 
+    public void setOnValueChangedListener(OnValueChangedListener listener) {
+        this.listener = listener;
+    }
+
     public interface OnValueChangedListener {
         void onValueChanged(float oldValue, float newValue);
     }
@@ -281,5 +275,23 @@ public class ScaleNumberPicker extends View implements ValueAnimator.AnimatorUpd
         design.setRoundedValues(false);
         invalidate();
         design.setRoundedValues(true);
+    }
+    
+    public void setOrientation(int orientation) {
+        if (orientation == HORIZONTAL) {
+            this.orientation = HORIZONTAL;
+            this.design = new HorizontalScale(attrs);
+        } else if (orientation == VERTICAL_LEFT) {
+            this.orientation = VERTICAL_LEFT;
+            this.design = new VerticalLeftScale(attrs);
+        } else if (orientation == VERTICAL_RIGHT){
+            this.orientation = VERTICAL_RIGHT;
+            this.design = new VerticalRightScale(attrs);
+        } else if (orientation == HORIZONTAL_CIRCULAR) {
+            this.orientation = HORIZONTAL_CIRCULAR;
+            this.design = new HorizontalCircularScale(attrs);
+        }
+        requestLayout();
+        invalidate();
     }
 }
--- app/src/main/java/com/arcm/scalenumberpicker/MainActivity.java
+++ app/src/main/java/com/arcm/scalenumberpicker/MainActivity.java
@@ -3,12 +3,46 @@ package com.arcm.scalenumberpicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.arcm.scalenumberpicker.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+    private ScaleNumberPicker scaleNumberPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setTitle("Demo");
+
+        this.scaleNumberPicker = binding.scaleNumberPicker;
+        binding.horizontal.setOnClickListener(this::onHorizontalOrientationSelected);
+        binding.verticalLeft.setOnClickListener(this::onVerticalLeftOrientationSelected);
+        binding.verticalRight.setOnClickListener(this::onVerticalRightOrientationSelected);
+        binding.horizontalCircular.setOnClickListener(this::onHorizontalCircularOrientationSelected);
+    }
+
+    private void onHorizontalOrientationSelected(View view) {
+        Log.i(TAG, "horizontal orientation button clicked");
+        scaleNumberPicker.setOrientation(ScaleNumberPicker.HORIZONTAL);
+    }
+
+    private void onHorizontalCircularOrientationSelected(View view) {
+        Log.i(TAG, "horizontal circular orientation button clicked");
+        scaleNumberPicker.setOrientation(ScaleNumberPicker.HORIZONTAL_CIRCULAR);
+    }
+
+    private void onVerticalLeftOrientationSelected(View view) {
+        Log.i(TAG, "vertical left orientation button clicked");
+        scaleNumberPicker.setOrientation(ScaleNumberPicker.VERTICAL_LEFT);
+    }
+
+    private void onVerticalRightOrientationSelected(View view) {
+        Log.i(TAG, "vertical right orientation button clicked");
+        scaleNumberPicker.setOrientation(ScaleNumberPicker.VERTICAL_RIGHT);
     }
 }
